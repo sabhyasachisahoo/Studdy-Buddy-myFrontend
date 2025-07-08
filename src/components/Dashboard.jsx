3b852f1523412f88c727a0637134af165f701a1c
Sabhyasachi Sahoo <sabhyasachisahoo16@gmail.com>
2025-07-09 02:29:26 +0530

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Card from "./ui/Card";
+import Card from "./ui/card";
 import  Progress  from "./ui/progress";
 import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer, Legend } from "recharts";
 // import { Avatar } from "./ui/avatar";
